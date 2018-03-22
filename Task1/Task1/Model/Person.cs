4fba791c55f830f6d8e994edfa40449fc58e22b1
TuomasVille <tuomas_kayhty@hotmail.com>
2018-03-22 11:14:24 +0200

    Task1 1.0

---
+++ Task1/Task1/Model/Person.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Task1.Model
+{
+    public partial class Person
+    {
+        public Person()
+        {
+            Phone = new HashSet<Phone>();
+        }
+
+        public long Id { get; set; }
+        [Column(TypeName = "nchar(10)")]
+        public string Name { get; set; }
+        public short? Age { get; set; }
+
+        [InverseProperty("Person")]
+        public ICollection<Phone> Phone { get; set; }
+    }
+}
